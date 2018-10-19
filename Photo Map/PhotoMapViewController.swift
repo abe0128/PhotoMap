d4134141b850c5a3fa6fe86ad6916c8824a26735
Abraham De Alba <abrahamdealba@Abrahams-MacBook-Pro.local>
2018-10-18 22:13:18 -0700

    minor error corrected

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -22,28 +22,17 @@ class PhotoAnnotation: NSObject, MKAnnotation
 
 class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, LocationsViewControllerDelegate, MKMapViewDelegate
 {
-    
-    func locationsPickedLocation(controller: LocationsViewController, latitude: NSNumber, longitude: NSNumber)
-    {
-        self.navigationController?.popToViewController(self, animated: true)
-        
-        let locationCoordinate = CLLocationCoordinate2DMake(latitude.doubleValue, longitude.doubleValue)
-        
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = locationCoordinate
-        annotation.title = "\(latitude), \(longitude)"
-        mapScreen.addAnnotation(annotation)
-    }
-    
+    var vc: UIImagePickerController!
 
     @IBOutlet weak var mapScreen: MKMapView!
     @IBOutlet weak var CameraButt: UIButton!
     
     @IBAction func onCameraButton(_ sender: Any)
     {
-        let vc = UIImagePickerController()
+        vc = UIImagePickerController()
         vc.delegate = self
         vc.allowsEditing = true
+        
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             print("Camera is available 📸")
             vc.sourceType = .camera
@@ -55,7 +44,6 @@ class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate,
             print("Camera 🚫 available so we will use photo library instead")
             vc.sourceType = .photoLibrary
             self.present(vc, animated: true, completion: nil)
-            
         }
         
     }
@@ -63,43 +51,25 @@ class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate,
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mapScreen.delegate = self
-        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
-                                              MKCoordinateSpanMake(0.1, 0.1))
+        
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1))
         mapScreen.setRegion(sfRegion, animated: false)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    
-    // MARK: - Navigation
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        let destinationVC = segue.destination as! LocationsViewController
-        // Pass the selected object to the new view controller.
-        destinationVC.delegate =  self
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController,
-                               didFinishPickingMediaWithInfo info: [String : Any]) {
-        // Get the image captured by the UIImagePickerController
-        _ = info[UIImagePickerControllerOriginalImage] as! UIImage
-        _ = info[UIImagePickerControllerEditedImage] as! UIImage
         
-        // Do something with the images (based on your use case)
-        
-        // Dismiss UIImagePickerController to go back to your original view controller
-        dismiss(animated: true, completion: {
-            self.performSegue(withIdentifier: "tagSegue", sender: nil)
-        })
+        mapScreen.delegate = self
     }
     
-    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+    func locationsPickedLocation(controller: LocationsViewController, latitude: NSNumber, longitude: NSNumber)
+    {
+        self.navigationController?.popToViewController(self, animated: true)
+        let locationCoordinate = CLLocationCoordinate2DMake(latitude.doubleValue, longitude.doubleValue)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locationCoordinate
+        annotation.title = "\(latitude), \(longitude)"
+        mapScreen.addAnnotation(annotation)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         let reuseID = "myAnnotationView"
         
         var annotationView = mapScreen.dequeueReusableAnnotationView(withIdentifier: reuseID)
@@ -114,5 +84,28 @@ class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate,
         
         return annotationView
     }
-
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: {
+            self.performSegue(withIdentifier: "tagSegue", sender: nil)
+        })
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        let destinationVC = segue.destination as! LocationsViewController
+        destinationVC.delegate =  self
+    }
 }
